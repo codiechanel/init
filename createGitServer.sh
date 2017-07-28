1845a1aebb4b9b423b4d95be04c3da10dca9ffe1
codiechanel <codiechanel@gmail.com>
2017-07-29 00:45:29 +0800

    Create createGitServer.sh

---
+++ createGitServer.sh
@@ -0,0 +1,12 @@
+sudo adduser git
+su git
+cd $HOME
+mkdir .ssh && chmod 700 .ssh
+touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys
+cat id_rsa.pub >> ~/.ssh/authorized_keys
+mkdir project.git
+cd project.git
+git init --bare
+
+# on remote client computer
+git remote add origin git@gitserver:/home/git/project.git
